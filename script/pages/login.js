b48a5032e486adda1916a42d51858f8a9e7ef965
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-03-28 10:41:06 +0700

    Perbaikan HTML

--- script/pages/login.js
+++ script/pages/login.js
@@ -1,30 +1,33 @@
 import UserRepository from "../repository/userRepository.js";
-$(document).ready(function () {
-  const userRepo = new UserRepository();
+$(document).ready(function() {
+    const userRepo = new UserRepository();
 
-  $("#login-form").submit(function (e) {
-    const button = $(this).find("button");
-    button.prop("disabled", true);
-    button.text("Loading...");
-    e.preventDefault();
-    let values = {};
-    $(this)
-      .serializeArray()
-      .forEach((element) => {
-        values[element.name] = element.value;
-      });
-    const user = {
-      email: values.email,
-      password: values.password,
-    };
-    userRepo.login(user).then(function (result) {
-      button.prop("disabled", false);
-      button.text("Sign in");
-      if (result.status) {
-        redirect_to("pages/dashboard");
-      } else {
-        alert(result.message);
-      }
+    $("#login-form").on("submit", function(e) {
+        e.preventDefault();
+
+        const button = $(this).find("button");
+        button.prop("disabled", true);
+        button.text("Loading...");
+
+        let values = {};
+        $(this)
+            .serializeArray()
+            .forEach((element) => {
+                values[element.name] = element.value;
+            });
+        const user = {
+            email: values.email,
+            password: values.password,
+        };
+        console.log(user);
+        userRepo.login(user).then(function(result) {
+            button.prop("disabled", false);
+            button.text("Sign in");
+            if (result.status) {
+                redirect_to("pages/dashboard");
+            } else {
+                alert(result.message);
+            }
+        });
     });
-  });
 });
