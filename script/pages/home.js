acafe61b8f680d75a1b295fd1cf9a1c70f70b216
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-03-29 08:04:17 +0700

    Ganti Index.html

--- script/pages/home.js
+++ script/pages/home.js
@@ -1,80 +1,161 @@
-$(document).ready(function () {
-  const itemLoad = () => {
-    $(".list-data").empty();
-    for (let i = 0; i < 4; i++) {
-      const item = `<div class="col-md-6"><div class="d-md-flex align-items-center">
-          <div class="col-md-12">
-              <div class="card  flex-md-row mb-4 shadow-sm h-md-250 skeleton">
-                  <div class="card-body d-flex flex-column align-items-start">
-                      <strong class="d-inline-block mb-2 text-primary  skeleton">BLOG</strong>
-                      <h3 class="mb-0">
-                          <a class="text-dark  skeleton" href="blog.html">Judul Blog</a>
-                      </h3>
-                      <div class="mb-1 text-muted  skeleton">23 Maret 2022</div>
-                      <p class="card-text mb-auto  skeleton">Deskripsi materi blog singkat.</p>
-                      <a class="skeleton" href="blog.html">Continue reading</a>
-                    
-                  </div>
-                  <img class="card-img-right flex-auto d-none d-lg-block img-thumb-blog" src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="Card image cap">
-              </div>
-          </div>
-      </div></div>`;
+$(document).ready(function() {
+    const itemLoad = () => {
+        $(".list-data").empty();
+        for (let i = 0; i < 4; i++) {
+            const item = `<div class="row my-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div  class="img-blog skeleton"></div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="py-3 content-blog">
+                                <h4 class="mb-0">
+                                    <a href="#" class="fw-bold text-dark skeleton">
+                                       Judul Ini Adalah
+                                    </a>
+                                </h4>
+                                <small class="text-muted skeleton">Kamis, 27 Agustus 2022</small>
+                                <div class="content-blog-card skeleton  mt-3 text-secondary">
+                                 
+                                </div>
+                                <p>
+                                    <a href="#" class="btn btn-primary skeleton">Read More</a>
+                                </p>
+                            </div>
 
-      $(".list-data").append(item);
-    }
-  };
+                        </div>
+                    </div>
 
-  const getBlogs = (search = "") => {
-    itemLoad();
-    const q = query(
-      collection(db, "blogs"),
-      where("title", ">=", search),
-      where("title", "<=", search + "~")
-    );
-
-    getDocs(q).then(async (querySnapshot) => {
-      $(".list-data").empty();
-      querySnapshot.forEach(async (doc) => {
-        const element = doc.data();
-        const user = await getDoc(element.author);
-        const label = await getDoc(element.category);
-        element.user = await user.data();
-        element.label = await label.id;
-        element.id = doc.id;
-
-        let item = `<div class="col-md-6">
-        <div class="d-md-flex align-items-center">
-        <div class="col-md-12">
-            <div class="card  flex-md-row mb-4 shadow-sm h-md-250">
-                <div class="card-body d-flex flex-column align-items-start">
-                    <strong class="d-inline-block mb-2 text-primary ">${
-                      element.label
-                    }</strong>
-                    <h3 class="mb-0">
-                        <a class="text-dark " href="blog.html">${
-                          element.title
-                        }</a>
-                    </h3>
-                    <small class="mb-1 text-muted ">${format_date(
-                      element.created_at
-                    )} </small>
-                    
-                    <div class="text-blog">${element.description}</div>
-                    <a href="blog.html?id=${element.id}">Lanjutkan Membaca</a>
-                  
                 </div>
-                <img class="card-img-right flex-auto d-none d-lg-block img-thumb-blog" src="${
-                  element.image
-                }" alt="Card image cap">
             </div>
-        </div>
-    </div>
         </div>`;
 
-        $(".list-data").append(item);
-      });
-    });
-  };
+            $("#list-blog").append(item);
+        }
+    };
 
-  getBlogs();
+    const getBlogs = (search = "") => {
+        itemLoad();
+        const q = query(
+            collection(db, "blogs"),
+            where("title", ">=", search),
+            where("title", "<=", search + "~")
+        );
+
+        getDocs(q).then(async(querySnapshot) => {
+            $("#list-blog").empty();
+            querySnapshot.forEach(async(doc) => {
+                const element = doc.data();
+                const user = await getDoc(element.author);
+                const label = await getDoc(element.category);
+                element.user = await user.data();
+                element.label = await label.id;
+                element.id = doc.id;
+                console.log(element);
+                let item = $(
+                    $.parseHTML(`<div class="row my-4">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="${
+                                  element.image
+                                }" alt="" class="img-blog">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="py-3 content-blog">
+                                    <h4 class="mb-0">
+                                        <a href="#" class="fw-bold text-dark">
+                                            ${element.title}
+                                        </a>
+                                    </h4>
+                                    <small class="text-muted">Kamis, 27 Agustus 2022</small>
+                                    <div class="content-blog-card  mt-3 text-secondary">
+                                       ${element.description} 
+                                    </div>
+                                    <p>
+                                        <a href="${base_url(
+                                          "blog",
+                                          "?id=" + element.id
+                                        )}" class="btn btn-primary">Read More</a>
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>`)
+                );
+                item.find(".content-blog-card *").removeAttr("style");
+                item.find(".content-blog-card *").css("font-size", "12pt");
+                item.find(".content-blog-card *").addClass("fw-light");
+                $("#list-blog").append(item);
+            });
+        });
+    };
+
+    const loadConfig = () => {
+        getDocs(collection(db, "pengaturan")).then((doc) => {
+            const config = {};
+            doc.forEach((snap) => {
+                config[snap.id] = snap.data().value;
+            });
+            $(".skeleton").removeClass("skeleton");
+            $("#judul_cover").text(config.judul_cover);
+            console.log(config);
+            $("#dekripsi_cover").text(config.dekripsi_cover);
+        });
+    };
+
+    const loadLabel = () => {
+        getDocs(collection(db, "labels")).then((doc) => {
+            $(".list-label").empty();
+            doc.forEach((snap) => {
+                const element = snap.data();
+                element.id = snap.id;
+                const item = `<a href="#" class="list-group-item list-group-item-action">${element.label}</a>`;
+                $(".list-label").append(item);
+            });
+        });
+    };
+
+    loadLabel();
+    loadConfig();
+    getBlogs();
+
+    window.onscroll = function() {
+        scrollFunction();
+    };
+    $("#tahun").text(new Date().getFullYear());
+    $(".navbar-toggler").click(function() {
+        $("#navbar").toggleClass("bg-sm-primary");
+    });
+
+    if (isLogin()) {
+        $(".btn-sign-in").html(`<i class = "fa fa-user me-2"> </i>Anggota`);
+        $(".btn-sign-in").attr("href", base_url("pages/dashboard"));
+    } else {
+        $(".btn-sign-in").html(`<i class = "fa fa-sign-in me-2"> </i>Login`);
+        $(".btn-sign-in").attr("href", base_url("masuk"));
+    }
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 150 ||
+            document.documentElement.scrollTop > 150
+        ) {
+            document.getElementById("navbar").classList.add("bg-primary");
+            document.querySelector(".text-cover").classList.add("d-none");
+            // document.getElementById("logo").style.fontSize = "25px";
+        } else {
+            document.getElementById("navbar").classList.remove("bg-primary");
+
+            document.querySelector(".text-cover").classList.remove("d-none");
+            // document.getElementById("logo").style.fontSize = "35px";
+        }
+    }
 });
